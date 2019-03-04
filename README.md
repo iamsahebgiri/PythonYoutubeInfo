4e528a4f455cdaa003d90f1e6c8d917e65547878
iamsahebgiri <iamsahebgiri@gmail.com>
2019-03-04 12:06:49 +0530

    added new features

---
+++ README.md
@@ -0,0 +1,11 @@
+## Youtube Channel Infromation
+
+Python based Console Application
+
+Obtain your Youttube Data API keys to use the app
+
+	Go to https://console.cloud.google.com/
+
+	Create a new Project
+
+	Generate API credentials
---
+++ YTInfo.py
@@ -0,0 +1,17 @@
+import  urllib.request
+import json
+
+cid = input("Enter Youtube Channel ID:")
+key = "AIzaSyD2uqDwFSHnl49h7u2Kyco2kMKygRFE8wY"
+
+data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id="+cid+"&key="+key).read().decode('utf-8')
+
+cdesc = json.loads(data)["items"][0]["snippet"]["description"]
+ctitle = json.loads(data)["items"][0]["snippet"]["title"]
+cviews = json.loads(data)["items"][0]["statistics"]["viewCount"]
+csubs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+
+print("Channel Name:",ctitle)
+print("Channel Decription:",cdesc)
+print("Total Views: {:,d}".format(int(cviews)))
+print("Total Subscribers: {:,d}".format(int(csubs)))
